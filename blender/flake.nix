8eb2fd1b51725d10daf0708557670d88e28ad893
Eelco Dolstra <edolstra@gmail.com>
2019-06-14 18:48:04 +0200

    blender_2_80: Bump

--- blender/flake.nix
+++ blender/flake.nix
@@ -63,10 +63,10 @@ in
 
       blender_2_80 = mkBlender {
         inherit pkgs;
-        name = "blender-bin-2.80-20190609-030c7df19da9";
+        name = "blender-bin-2.80-20190609-5e626e766459";
         src = import <nix/fetchurl.nix> {
-          url = https://builder.blender.org/download/blender-2.80-030c7df19da9-linux-glibc224-x86_64.tar.bz2;
-          hash = "sha256-awi5URUcvJNuJdC9esrIiQYAgfFRKvcsrRiHm7A5VW8=";
+          url = https://builder.blender.org/download/blender-2.80-5e626e766459-linux-glibc224-x86_64.tar.bz2;
+          hash = "sha256-JXyRFtUJ5ZmWMOBb65oIUEPf9yEwr2tNJ3USvAp7RdQ=";
         };
       };
 
