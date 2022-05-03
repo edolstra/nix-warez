0b3312d1441099e2b8e137ed52def0af0f7b4c1e
lucasew <lucas59356@gmail.com>
2022-05-03 17:57:34 -0300

    blender: use pname and version + pass nix check
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- blender/flake.nix
+++ blender/flake.nix
@@ -9,14 +9,14 @@
 
       pkgs = import nixpkgs {
         system = "x86_64-linux";
-        overlays = [ self.overlay ];
+        overlays = [ self.overlays.default ];
       };
 
-      mkBlender = { name, src }:
+      mkBlender = { pname, version, src }:
         with pkgs;
 
         stdenv.mkDerivation rec {
-          inherit name src;
+          inherit pname version src;
 
           buildInputs = [ makeWrapper ];
 
@@ -55,10 +55,11 @@
 
     in {
 
-      overlay = final: prev: {
+      overlays.default = final: prev: {
 
         blender_2_79 = mkBlender {
-          name = "blender-bin-2.79-20190523-054dbb833e15";
+          pname = "blender-bin";
+          version = "2.79-20190523-054dbb833e15";
           src = import <nix/fetchurl.nix> {
             url = https://builder.blender.org/download/blender-2.79-054dbb833e15-linux-glibc224-x86_64.tar.bz2;
             hash = "sha256-/qbRx4KKiJBka84M4iXB8z3PKzqBIuWG5Zihyf//QTU=";
@@ -66,7 +67,8 @@
         };
 
         blender_2_81 = mkBlender {
-          name = "blender-bin-2.81a";
+          pname = "blender-bin";
+          version = "2.81a";
           src = import <nix/fetchurl.nix> {
             url = https://ftp.nluug.nl/pub/graphics/blender/release/Blender2.81/blender-2.81a-linux-glibc217-x86_64.tar.bz2;
             hash = "sha256-CNcYUF0esdJh77qWsHhyIKdtNXzluUrKEI/J4MM51sY=";
@@ -74,7 +76,8 @@
         };
 
         blender_2_82 = mkBlender {
-          name = "blender-bin-2.82a";
+          pname = "blender-bin";
+          version = "2.82a";
           src = import <nix/fetchurl.nix> {
             url = https://ftp.nluug.nl/pub/graphics/blender/release/Blender2.82/blender-2.82a-linux64.tar.xz;
             hash = "sha256-+0ACWBIlJcUaWJcZkZfnQBBJT3HyshIsTdEiMk5u3r4=";
@@ -82,7 +85,8 @@
         };
 
         blender_2_83 = mkBlender {
-          name = "blender-bin-2.83.9";
+          pname = "blender-bin";
+          version = "2.83.9";
           src = import <nix/fetchurl.nix> {
             url = https://ftp.nluug.nl/pub/graphics/blender/release/Blender2.83/blender-2.83.9-linux64.tar.xz;
             hash = "sha256-xL1i71xw3dDYPUxHl9SRsoeqBDME8QHo3tIgeLE64Oo=";
@@ -90,7 +94,8 @@
         };
 
         blender_2_90 = mkBlender {
-          name = "blender-bin-2.90.1";
+          pname = "blender-bin";
+          version = "2.90.1";
           src = import <nix/fetchurl.nix> {
             url = https://ftp.nluug.nl/pub/graphics/blender/release/Blender2.90/blender-2.90.1-linux64.tar.xz;
             hash = "sha256-BUZoxGo+VpIfKDcJ9Ro194YHhhgwAc8uqb4ySdE6xmc=";
@@ -98,7 +103,8 @@
         };
 
         blender_2_91 = mkBlender {
-          name = "blender-bin-2.91.2";
+          pname = "blender-bin";
+          version = "2.91.2";
           src = import <nix/fetchurl.nix> {
             url = https://ftp.nluug.nl/pub/graphics/blender/release/Blender2.91/blender-2.91.2-linux64.tar.xz;
             hash = "sha256-jx4eiFJ1DhA4V5M2x0YcGlSS2pc84Yjh5crpmy95aiM=";
@@ -106,7 +112,8 @@
         };
 
         blender_2_92 = mkBlender {
-          name = "blender-bin-2.92.0";
+          pname = "blender-bin";
+          version = "2.92.0";
           src = import <nix/fetchurl.nix> {
             url = https://ftp.nluug.nl/pub/graphics/blender/release/Blender2.92/blender-2.92.0-linux64.tar.xz;
             hash = "sha256-LNF61unWwkGsFLhK1ucrUHruyXnaPZJrGhRuiODrPrQ=";
@@ -114,7 +121,8 @@
         };
 
         blender_2_93 = mkBlender {
-          name = "blender-bin-2.93.7";
+          pname = "blender-bin";
+          version = "2.93.7";
           src = import <nix/fetchurl.nix> {
             url = https://ftp.nluug.nl/pub/graphics/blender/release/Blender2.93/blender-2.93.7-linux-x64.tar.xz;
             hash = "sha256-ZNKWs5XJrlGI9Cm/F5R3Uxy+R/WVyPIg2DYdwt1pYuY=";
@@ -122,7 +130,8 @@
         };
 
         blender_3_0 = mkBlender {
-          name = "blender-bin-3.0.1";
+          pname = "blender-bin";
+          version = "3.0.1";
           src = import <nix/fetchurl.nix> {
             url = https://ftp.nluug.nl/pub/graphics/blender/release/Blender3.0/blender-3.0.1-linux-x64.tar.xz;
             hash = "sha256-TxeqPRDtbhPmp1R58aUG9YmYuMAHgSoIhtklTJU+KuU=";
@@ -130,7 +139,8 @@
         };
 
         blender_3_1 = mkBlender {
-          name = "blender-bin-3.1.2";
+          pname = "blender-bin";
+          version = "3.1.2";
           src = import <nix/fetchurl.nix> {
             url = https://ftp.nluug.nl/pub/graphics/blender/release/Blender3.1/blender-3.1.2-linux-x64.tar.xz;
             hash = "sha256-wdNFslxvg3CLJoHTVNcKPmAjwEu3PMeUM2bAwZ5UKVg=";
@@ -139,7 +149,7 @@
 
       };
 
-      packages.x86_64-linux = {
+      packages.x86_64-linux = rec {
         inherit (pkgs)
           blender_2_79
           blender_2_81
@@ -151,9 +161,8 @@
           blender_2_93
           blender_3_0
           blender_3_1;
+        default = blender_3_1;
       };
-
-      defaultPackage.x86_64-linux = self.packages.x86_64-linux.blender_3_1;
 
       checks.x86_64-linux = {
         blender_2_79 = mkTest { blender = self.packages.x86_64-linux.blender_2_79; };
