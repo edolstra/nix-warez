8ae661516ca53314cfad8a8d3ab41e2274fdc168
Eelco Dolstra <edolstra@gmail.com>
2025-08-22 22:55:46 +0200

    blender-bin: 4.5.1 -> 4.5.2

--- blender/flake.nix
+++ blender/flake.nix
@@ -248,10 +248,10 @@
 
         blender_4_5 = mkBlender {
           pname = "blender-bin";
-          version = "4.5.1";
+          version = "4.5.2";
           src = import <nix/fetchurl.nix> {
-            url = "https://ftp.nluug.nl/pub/graphics/blender/release/Blender4.5/blender-4.5.1-linux-x64.tar.xz";
-            hash = "sha256-CFp+1O2Aw8tmeDutdvI285iX3l0ziEq9Ez4MbblMDxQ=";
+            url = "https://ftp.nluug.nl/pub/graphics/blender/release/Blender4.5/blender-4.5.2-linux-x64.tar.xz";
+            hash = "sha256-u6U2GSlJHM1pN0uTxUfwh/CcnqrF2MyW2jIQSFwT1E8=";
           };
         };
       };
